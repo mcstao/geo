41098a8b7eb075c38d20edc4afb098fb0182d5f1
mcstao <arislg99@gmail.com>
2025-02-04 17:31:11 +0600

    Added map for place in admin

--- place/admin.py
+++ place/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
+from django.contrib.gis.admin.options import GISModelAdmin
 from place.models import Place, Review
 
-admin.site.register(Place)
+@admin.register(Place)
+class PlaceAdmin(GISModelAdmin):
+    default_zoom = 10
+
 admin.site.register(Review)
+
