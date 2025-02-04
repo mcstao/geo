c2e0e8ba0b050e519faa4dcd48262f7edb03a011
mcstao <arislg99@gmail.com>
2025-02-04 13:13:28 +0600

    Start new app

--- place/models.py
+++ place/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Place(models.Model):
+    location = models.PointField()
+
+    def __str__(self):
+        return f"({self.location.y}, {self.location.x})"
+
+
+class Review(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    text = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"({self.place})"
